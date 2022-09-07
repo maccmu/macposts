7e3fc61c08168be1e37f46555fcd8c34cca382f3
Pengji Zhang <pengji@babyam>
2022-09-07 15:48:15 -0400

    Reformat Python files
    * macposts/_compat.py: reformat with black.

--- macposts/_compat.py
+++ macposts/_compat.py
@@ -4,10 +4,7 @@ This module provides aliases and obsolete definitions to avoid breakage.
 
 """
 
-__all__ = [
-    "Dta_Api",
-    "Mcdta_Api"
-]
+__all__ = ["Dta_Api", "Mcdta_Api"]
 
 from _macposts_ext import Dta as Dta_Api
 from _macposts_ext import Mcdta as Mcdta_Api
