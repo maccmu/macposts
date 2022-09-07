c886e32c7474cad30da7a930edf3a5efd6e91dab
Pengji Zhang <pengji@babyam>
2022-09-07 15:47:34 -0400

    Suppress flake8 warnings
    * macposts/__init__.py: suppress flake8 warnings.

--- macposts/__init__.py
+++ macposts/__init__.py
@@ -7,5 +7,5 @@ modeling.
 
 __version__ = "0.0.1"
 
-from _macposts_ext import Dta, Mcdta
-from ._compat import *
+from _macposts_ext import Dta, Mcdta  # noqa: F401
+from ._compat import *  # noqa: F401,F403
