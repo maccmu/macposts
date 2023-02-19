dd43810f30ed6c324cd1d410a2ef3489ce816df5
Pengji Zhang <pengji@babyam>
2023-02-18 19:26:45 -0500

    Bump version
    * macposts/__init__.py: version 0.2.1-dev1 → 0.2.1-dev2.

--- macposts/__init__.py
+++ macposts/__init__.py
@@ -5,7 +5,7 @@ modeling.
 
 """
 
-__version__ = "0.2.1-dev1"
+__version__ = "0.2.1-dev2"
 
 from _macposts_ext import Dta, Mcdta, set_random_state  # noqa: F401
 from ._compat import *  # noqa: F401,F403
