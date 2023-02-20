cc4b68c3babeac7a532316005659911269fae3af
Pengji Zhang <pengji@babyam>
2023-02-20 15:25:16 -0500

    Release v0.2.1
    This release mainly brings a few improvements on the configuration and data
    readers. Now they are less surprising. Note that the future plan is to remove
    those reader completely from libmacposts, so we probably will not improve it any
    further.
    
    Also in this release we introduce some basic test cases. They cannot guarantee
    the correctness of the library, but could bring a bit of confidence while
    developing.
    
    * macposts/__init__.py: bump version 0.2.1-dev2 → 0.2.1.

--- macposts/__init__.py
+++ macposts/__init__.py
@@ -5,7 +5,7 @@ modeling.
 
 """
 
-__version__ = "0.2.1-dev2"
+__version__ = "0.2.1"
 
 from _macposts_ext import Dta, Mcdta, set_random_state  # noqa: F401
 from ._compat import *  # noqa: F401,F403
