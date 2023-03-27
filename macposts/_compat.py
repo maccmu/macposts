f30a1cafb6fbd725378860da17f100d13ac07f2c
Pengji Zhang <pengji@babyam>
2023-03-27 16:13:39 -0400

    Bring in more compatibility aliases
    In [1: 046f43b] we merged the patch from Qiling, who introduced more alternative
    names to the classes. To maintain compatibility, we add those aliases.
    
    * macposts/_compat.py: Add more aliases.
    
    1: 2023-03-23 046f43b45872e708ff86cef009e8a0ebd20c36df
       Install accumulated changes

--- macposts/_compat.py
+++ macposts/_compat.py
@@ -4,7 +4,21 @@ This module provides aliases and obsolete definitions to avoid breakage.
 
 """
 
-__all__ = ["Dta_Api", "Mcdta_Api"]
+__all__ = [
+    "Dta_Api",
+    "dta_api",
+    "Mcdta_Api",
+    "mcdta_api",
+    "tdsp_api",
+    "mmdta_api",
+]
 
-from _macposts_ext import Dta as Dta_Api
-from _macposts_ext import Mcdta as Mcdta_Api
+from _macposts_ext import (
+    Dta as Dta_Api,
+    Mcdta as Mcdta_Api,
+    Tdsp as tdsp_api,
+    Mmdta as mmdta_api,
+)
+
+dta_api = Dta_Api
+mcdta_api = Mcdta_Api
