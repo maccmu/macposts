6e6fafca17cc42239b1d199f86b9e371f9b34b1d
Pengji Zhang <pengji@babyam>
2023-03-27 18:43:19 -0400

    Remove an outdated note
    We have fixed that issue in our fork of SNAP.
    
    * macposts/_macposts_ext.cpp: Remove a note that is no longer true.

--- macposts/_macposts_ext.cpp
+++ macposts/_macposts_ext.cpp
@@ -1,9 +1,3 @@
-// XXX: On macOS with the stock Clang compiler, including 'pybind11/eigen.h'
-// after 'Snap.h' will cause a nasty compilation error because Snap defines an
-// overloaded '!=' operator and the compiler is confused. So here is a quick
-// and dirty fix: include 'pybind11/eigen.h' first. However, we probably need
-// to find a real solution.
-
 #include <pybind11/eigen.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
