690c9d457829ddf3610a0102891fd563787b49b4
Pengji Zhang <pengji@babyam>
2023-06-01 18:08:51 -0400

    Add new random functions
    * macposts/_ext/utils.h: Add a default random number generator and modify/add
    random functions to use it.

--- macposts/_ext/utils.h
+++ macposts/_ext/utils.h
@@ -1,6 +1,9 @@
 // Supporting utilities and common constants.
 #pragma once
 
+#include <algorithm>
+#include <random>
+
 namespace MNM_Ults
 {
 void set_random_state (unsigned int s);
@@ -8,6 +11,8 @@ void set_random_state (unsigned int s);
 
 namespace macposts
 {
+// The default random number generator.
+static std::mt19937 rng;
 // FIXME: Upper bound for travel time (?).
 static const double TT_UPPER_BOUND = 20;
 
@@ -17,6 +22,21 @@ inline void
 set_random_state (unsigned int s)
 {
   MNM_Ults::set_random_state (s);
+  rng.seed (s);
+}
+
+template <typename RandomIt>
+inline void
+random_shuffle (RandomIt first, RandomIt last)
+{
+  random_shuffle (first, last, rng);
+}
+
+template <typename RandomIt, typename RandomGen>
+inline void
+random_shuffle (RandomIt first, RandomIt last, RandomGen &&rng)
+{
+  std::shuffle (first, last, rng);
 }
 }
 }
